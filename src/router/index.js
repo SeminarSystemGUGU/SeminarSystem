8bd6c3716af714ec6b37d0b9b6339276b3c8ec7a
ShengzheWang <24320162202917@stu.xmu.edu.cn>
2018-11-22 17:12:03 +0800

    全部推倒重做

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ConfirmLogin from '../components/BeforeConfirm/ConfirmLogin'
-import AppBar from '../components/ReuseComponents/AppBar'
-import FindPassword from '../components/BeforeConfirm/FindPassword'
-import SetPassword from '../components/BeforeConfirm/SetPassword'
-import MainPage from '../components/TeacherPages/MainPage'
-import AccountSetting from '../components/TeacherPages/AccountSetting'
-import Setting from '../components/TeacherPages/Setting'
-import SeminarMainPage from '../components/Seminar/SeminarMainPage'
 import NewLogIn from '../components/NewLogIn'
 
 Vue.use(Router);
@@ -15,40 +7,8 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
-      path:'/SeminarMainPage',
-      component:SeminarMainPage
-    },
-    {
-      path:'/Setting',
-      component:Setting
-    },
-    {
-      path:'/AccountSetting',
-      component:AccountSetting
-    },
-    {
-      path:'/MainPage',
-      component:MainPage
-    },
-    {
-      path:'/SetPassword',
-      component:SetPassword
-    },
-    {
-      path:'/FindPassword',
-      component:FindPassword
-    },
-    {
-      path:'/AppBar',
-      component:AppBar
-    },
-    {
-      path:'/ConfirmLogin',
-      component:ConfirmLogin
-    },
-    {
       path: '/',
-      name: 'HelloWorld',
+      name: 'Login',
       component: NewLogIn
     }
   ]
