212b1843de6a4e7fb52c07008f7c452e071863b6
ShengzheWang <24320162202917@stu.xmu.edu.cn>
2018-11-27 19:26:31 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,11 @@ import TeacherStuTeams from '../components/TeacherPages/TeacherStuTeams'
 import TeacherMyCourses from '../components/TeacherPages/TeahcerMyCourses'
 import TeacherCourseGrades from '../components/TeacherPages/TeacherCourseGrades'
 import TeacherCourseDetails from '../components/TeacherPages/TeacherCourseDetails'
+import TeacherClassInfos from '../components/TeacherPages/TeacherClassInfos'
+import NewClassPage from '../components/TeacherPages/NewClassPage'
+
+
+import CreateTeam from '../components/StudentPages/CreateTeam'
 
 import StuMyCourses from '../components/StudentPages/MyCourses'
 import StuMainSeminars from '../components/Studentpages/MainSeminars'
@@ -75,11 +80,23 @@ export default new Router({
       path:'/TeacherCourseDetails',
       component:TeacherCourseDetails
     },
+    {
+      path:'/TeacherClassInfos',
+      component:TeacherClassInfos
+    },
+    {
+      path:'/NewClass',
+      component:NewClassPage
+    },
 
     //学生端组件
     {
       path:'/StuMyCourses',
       component:StuMyCourses
+    },
+    {
+      path:'/CreateTeam',
+      component:CreateTeam
     },
     {
       path:'/StuMainSeminars',
