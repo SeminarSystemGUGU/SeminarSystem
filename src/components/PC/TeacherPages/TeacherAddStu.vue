b576255098c96b5ac2a190af338321fae71f851d
ShengzheWang <24320162202917@stu.xmu.edu.cn>
2018-12-11 20:54:29 +0800

    更新组件

--- src/components/PC/TeacherPages/TeacherAddStu.vue
+++ src/components/PC/TeacherPages/TeacherAddStu.vue
@@ -9,13 +9,18 @@
         <span>班级列表</span>
       </div>
       <div class="class-table">
-        <el-table :data="table">
+        <el-table stripe border :data="table" class="data-table">
           <el-table-column prop="className" label="班级名称">
           </el-table-column>
-          <el-table-column prop="classForm">
+          <el-table-column prop="classForm" label="班级名单文件">
+            <template slot-scope="scope">
+            </template>
           </el-table-column>
-          <el-table-column>
-
+          <el-table-column label="操作">
+            <template slot-scope="scope">
+              <el-button type="primary" size="mini">提交</el-button>
+              <el-button type="primary" size="mini">重置</el-button>
+            </template>
           </el-table-column>
 
         </el-table>
@@ -31,7 +36,11 @@
           return{
             table:[
               {
-                className:'',
+                className:'一班',
+                classForm:'',
+              },
+              {
+                className:'二班',
                 classForm:'',
               }
             ]
@@ -57,11 +66,40 @@
   }
 
   .main-content{
+
+    .data-table{
+      width: 95%;
+      border: none;
+      border-top-color: #182f63;
+      border-top-width: 1.5px;
+
+    }
+
+    .el-table__body {
+      border-color: #182f63;
+      border-width: 1.5px;
+      font-size: 15px;
+      .el-table__row {
+        background: #F6F7FB;
+      }
+      .el-table__row--striped {
+        background: #ffffff !important;
+        td {
+          background: #ffffff !important;
+        }
+      }
+    }
+
+
     margin-top: 20px;
 
     .main-title{
       text-align: left;
       font-size: 20px;
+    }
+
+    .class-table{
+      margin-top: 10px;
     }
   }
 }
